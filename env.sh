131e0a65302e35498d6b4c4329cb106cec6c810b
최형욱[Semantic Perception] <hyungwook.choi.1@navercorp.com>
2025-07-28 06:29:41 +0000

    update flash attn ver

--- env.sh
+++ env.sh
@@ -8,4 +8,4 @@ uv pip install -e ".[train]"
 
 cd ..
 uv pip install -e .
-MAX_JOBS=4 uv pip install "flash-attn<2.8.0" --no-build-isolation
+MAX_JOBS=4 uv pip install "flash-attn<=2.7.4.post1" --no-build-isolation
